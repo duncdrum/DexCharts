cc3ca0d70c62660750828ea9ecc9f1643575f882
Patrick Martin <patrick145@msn.com>
2013-11-15 18:35:00 -0500

    Regenerated

--- dex.js
+++ dex.js
@@ -23,19 +23,17 @@ dex.config = {};
  * foo.bar into a structure { foo : { bar : value}}.  It will delete
  * the hierarchical name and overwrite the value into the proper
  * location leaving any previous object properties undisturbed.
- *  
+ *
  * @param {Object} config The configuration which we will expand.
- * 
+ *
  */
-dex.config.expand = function(config)
-{
+dex.config.expand = function (config) {
   var name,
-      ci,
-      expanded;
+    ci,
+    expanded;
 
   // We have nothing, return nothing.
-  if (!config)
-  {
+  if (!config) {
     return config;
   }
 
@@ -43,19 +41,15 @@ dex.config.expand = function(config)
   expanded = dex.object.clone(config);
 
   // Iterate over the property names.
-  for (name in config)
-  {
+  for (name in config) {
     // If this is our property the process it, otherwise ignore.
-    if (config.hasOwnProperty(name))
-    {
+    if (config.hasOwnProperty(name)) {
       // The property name is non-null.
-      if (name)
-      {
+      if (name) {
         // Determine character index.
         ci = name.indexOf('.');
       }
-      else
-      {
+      else {
         // Default to -1
         ci = -1;
       }
@@ -63,8 +57,7 @@ dex.config.expand = function(config)
       // if Character index is > -1, we have a hierarchical name.
       // Otherwise do nothing, copying was already handled in the
       // cloning activity.
-      if (ci > -1)
-      {
+      if (ci > -1) {
         // Set it...
         dex.object.setHierarchical(expanded, name,
           dex.object.clone(expanded[name]), '.');
@@ -78,22 +71,24 @@ dex.config.expand = function(config)
   return expanded;
 };
 
-dex.config.font = function(custom)
-{
-  var config = 
+dex.config.expandAndOverlay = function (top, bottom) {
+  return dex.object.overlay(dex.config.expand(top), dex.config.expand(bottom));
+}
+
+dex.config.font = function (custom) {
+  var config =
   {
-    'size'    : 18,
-    'family'  : 'sans-serif',
-    'style'   : 'normal',
-    'variant' : 'normal',
-    'weight'  : 'normal'
+    'size': 18,
+    'family': 'sans-serif',
+    'style': 'normal',
+    'variant': 'normal',
+    'weight': 'normal'
   };
 
   return (custom) ? dex.object.overlay(custom, config) : config;
 };
 
-dex.config.configureFont = function(node, config)
-{
+dex.config.configureFont = function (node, config) {
   return node
     .attr("font-family", dex.config.optionValue(config.family))
     .attr("font-weight", dex.config.optionValue(config.weight))
@@ -101,239 +96,252 @@ dex.config.configureFont = function(node, config)
     .style("font-size", dex.config.optionValue(config.size));
 };
 
-dex.config.tick = function(custom)
-{
+dex.config.tick = function (custom) {
   var config =
   {
-    'count' : 5,
+    'count': 5,
     //'tickValues'  : null,
-    'subdivide' : 3,
-    'size' :
-    {
-      'major' : 5,
-      'minor' : 3,
-      'end'   : 5
+    'subdivide': 3,
+    'size': {
+      'major': 5,
+      'minor': 3,
+      'end': 5
     },
-    'padding' : 5,
-    'format' : d3.format(",d"),
-    'label' : this.label()
+    'padding': 5,
+    'format': d3.format(",d"),
+    'label': this.label()
   };
-  if (custom)
-  {
-  	config = dex.object.overlay(custom, config);
+  if (custom) {
+    config = dex.object.overlay(custom, config);
   }
   return config;
 };
 
-dex.config.scale = function(custom)
-{
+dex.config.xaxis = function (custom) {
   var config =
   {
-    'scale' : d3.scale.linear(),
-    'type'  : 'linear'
+    'scale': d3.scale.linear(),
+    'orient': "bottom",
+    'tick': this.tick(),
+    'label': this.label()
   };
-  if (custom)
-  {
-    config = dex.object.overlay(custom, config);
-  }
-  return config;
-}
-
-dex.config.xaxis = function(custom)
-{
-	var config =
-  {
-    'scale'  : d3.scale.linear(),
-    'orient' : "bottom",
-    'tick'   : this.tick(),
-    'label'  : this.label()
-  };
-  if (custom)
-  {
+  if (custom) {
     config = dex.object.overlay(custom, config);
   }
   return config;
 };
 
-dex.config.yaxis = function(custom)
-{
-  var config = 
-	{
-    'scale'  : d3.scale.linear(),
-    'orient' : 'left',
-    'tick'   : this.tick(),
-    'label'  : this.label({'transform' : 'rotate(-90)'})
-  };
-  if (custom)
+dex.config.yaxis = function (custom) {
+  var config =
   {
-  	config = dex.object.overlay(custom, config);
+    'scale': d3.scale.linear(),
+    'orient': 'left',
+    'tick': this.tick(),
+    'label': this.label({'transform': 'rotate(-90)'})
+  };
+  if (custom) {
+    config = dex.object.overlay(custom, config);
   }
   return config;
 };
 
-dex.config.stroke = function(custom)
-{
-  var config = 
-	{
-    'width'     : 1,
-    'color'     : "black",
-    'opacity'   : 1,
-    'dasharray' : ''
-  };
-  if (custom)
+dex.config.stroke = function (custom) {
+  var defaults =
   {
-  	config = dex.object.overlay(custom, config);
-  }
+    'width': 1,
+    'color': "black",
+    'opacity': 1,
+    'dasharray': ''
+  };
+
+  var config = dex.config.expandAndOverlay(custom, defaults);
   return config;
 };
 
-dex.config.optionValue = function(option)
-{
-  return function(d, i)
-  {
-     if (dex.object.isFunction(option))
-     {
-       return option(d, i);
-     }
-     else
-     {
-       return option;
-     }
+dex.config.optionValue = function (option) {
+  return function (d, i) {
+    if (dex.object.isFunction(option)) {
+      return option(d, i);
+    }
+    else {
+      return option;
+    }
   };
 }
 
-dex.config.configureStroke = function(node, config)
-{
-  dex.config.setStyle(node,'stroke-width', config.width);
-  dex.config.setStyle(node,'stroke', config.color);
-  dex.config.setStyle(node,'stroke-opacity', config.opacity);
-  dex.config.setStyle(node,'stroke-dasharray', config.dasharray);
-  
+dex.config.configureStroke = function (node, config) {
+  dex.config.setStyle(node, 'stroke-width', config.width);
+  dex.config.setStyle(node, 'stroke', config.color);
+  dex.config.setStyle(node, 'stroke-opacity', config.opacity);
+  dex.config.setStyle(node, 'stroke-dasharray', config.dasharray);
+
   return node;
 };
 
+dex.config.fill = function (custom) {
+  var defaults =
+  {
+    'fillColor': "blue",
+    'fillOpacity': 1
+  };
 
-dex.config.configureFill = function(node, config)
-{
-  dex.config.setStyle(node,'fill', config.fill);
-  dex.config.setStyle(node,'fill-opacity', config.fillOpacity);
-  return node
-    .style('fill', dex.config.optionValue(config.fill))
-    .style('fill-opacity', dex.config.optionValue(config.fillOpacity));
+  var config = dex.config.expandAndOverlay(custom, defaults);
+  return config;
 };
 
-dex.config.configureLink = function(node, config)
-{
+dex.config.configureFill = function (node, config) {
+  dex.console.log("configureFill", node, config);
+  dex.config.setAttr(node, 'fill', config.fillColor);
+  dex.config.setAttr(node, 'fill-opacity', config.fillOpacity)
+    .style("fill-opacity", config.fillOpacity);
+  return node;
+};
+
+dex.config.link = function (custom) {
+  var defaults =
+  {
+    'fill': this.fill(),
+    'stroke': this.stroke(),
+    'transform' : ''
+    //'d' : null
+  };
+
+  var config = dex.config.expandAndOverlay(custom, defaults);
+  return config;
+};
+
+dex.config.configureLink = function (node, config) {
   dex.config.configureStroke(node, config.stroke);
-  dex.config.configureFill(node, config);
+  dex.config.configureFill(node, config.fill);
+  dex.config.setAttr(node, 'transform', config.transform);
+  dex.config.setAttr(node, 'd', config.d);
+  dex.console.log("LINK", "NODE", node, "CONFIG", config);
+
   return node;
 }
 
-dex.config.rectangle = function(custom)
-{
-  var config = 
-	{
-    'width'   : 50,
-    'height'  : 50,
-    'x'       : 0,
-    'y'       : 0,
-    'rx'      : 0,
-    'ry'      : 0,
-    'stroke'  : dex.config.stroke(),
-    'opacity' : 1,
-    'color'   : d3.scale.category20()
+dex.config.rectangle = function (custom) {
+  var config =
+  {
+    'width': 50,
+    'height': 50,
+    'x': 0,
+    'y': 0,
+    'rx': 0,
+    'ry': 0,
+    'stroke': dex.config.stroke(),
+    'opacity': 1,
+    'color': d3.scale.category20()
   };
-  if (custom)
-  {
-  	config = dex.object.overlay(custom, config);
+  if (custom) {
+    config = dex.object.overlay(custom, config);
   }
- return config;
+  return config;
 };
 
-dex.config.configureRectangle = function(node, config)
-{
-	  dex.config.setAttr(node, 'width', config.width);
-	  dex.config.setAttr(node, 'height', config.height);
-	  dex.config.setAttr(node, 'x', config.x);
-	  dex.config.setAttr(node, 'y', config.y);
-    dex.config.setAttr(node, 'rx', config.rx);
-    dex.config.setAttr(node, 'ry', config.ry);
-	  dex.config.setAttr(node, 'opacity', config.opacity);
-	  dex.config.setAttr(node, 'fill', config.color);
+dex.config.configureRectangle = function (node, config) {
+  dex.config.setAttr(node, 'width', config.width);
+  dex.config.setAttr(node, 'height', config.height);
+  dex.config.setAttr(node, 'x', config.x);
+  dex.config.setAttr(node, 'y', config.y);
+  dex.config.setAttr(node, 'rx', config.rx);
+  dex.config.setAttr(node, 'ry', config.ry);
+  dex.config.setAttr(node, 'opacity', config.opacity);
+  dex.config.setAttr(node, 'fill', config.color);
 
-	  return node.call(dex.config.configureStroke, config.stroke);
+  return node.call(dex.config.configureStroke, config.stroke);
 };
 
-dex.config.setAttr = function(node, name, value)
-{
-  if (typeof value != 'undefined')
+dex.config.line = function (custom) {
+  var defaults =
   {
+    'start': dex.config.point(),
+    'end': dex.config.point(),
+    'stroke': dex.config.stroke()
+  };
+  var config = dex.config.expandAndOverlay(custom, defaults);
+  return config;
+};
+
+dex.config.configureLine = function (node, config) {
+
+  dex.config.setAttr(node, 'x1', config.start.x);
+  dex.config.setAttr(node, 'y1', config.start.y);
+  dex.config.setAttr(node, 'x2', config.end.x);
+  dex.config.setAttr(node, 'y2', config.end.y);
+  dex.config.configureStroke(node, config.stroke);
+
+  return node;
+};
+
+dex.config.setAttr = function (node, name, value) {
+  if (typeof value != 'undefined') {
+    //dex.console.log("Set Attr: '" + name + "'='" + value + "'");
     node.attr(name, dex.config.optionValue(value));
   }
+  else
+  {
+    //dex.console.log("Undefined Attr: '" + name + "'='" + value + "'");
+  }
   return node;
 }
 
-dex.config.setStyle = function(node, name, value)
-{
-  if (typeof value != 'undefined')
-  {
+dex.config.setStyle = function (node, name, value) {
+  if (typeof value !== 'undefined') {
+    //dex.console.log("Set Style: '" + name + "'='" + dex.config.optionValue(value) + "'");
     node.style(name, dex.config.optionValue(value));
   }
+  else
+  {
+    //dex.console.log("Undefined Style: '" + name + "'='" + value + "'");
+  }
   return node;
 }
 
-dex.config.point = function(custom)
-{
-  var config = 
+dex.config.point = function (custom) {
+  var config =
   {
-    'x'  : 0,
-    'y'  : 0
+    'x': 0,
+    'y': 0
   };
-  if (custom)
-  {
+  if (custom) {
     config = dex.object.overlay(custom, config);
   }
- return config;
+  return config;
 };
 
-dex.config.configurePoint = function(node, config)
-{
+dex.config.configurePoint = function (node, config) {
   return node
     .attr('x', config.center.cx)
     .attr('y', config.center.cy);
 };
 
-dex.config.circle = function(custom)
-{
-  var config = 
+dex.config.circle = function (custom) {
+  var config =
   {
-    'center'  : dex.config.point(),
-    'radius'  : 10,
-    'style'   :
-    {
-      'stroke'  : dex.config.stroke(),
-      'color'   : d3.scale.category20(),
-      'opacity' : 1.0
+    'center': dex.config.point(),
+    'radius': 10,
+    'style': {
+      'stroke': dex.config.stroke(),
+      'color': d3.scale.category20(),
+      'opacity': 1.0
     }
   };
-  if (custom)
-  {
+  if (custom) {
     config = dex.object.overlay(custom, config);
   }
- return config;
+  return config;
 };
 
 // Configures: opacity, color, stroke.
-dex.config.configureShapeStyle = function(node, config)
-{
+dex.config.configureShapeStyle = function (node, config) {
   return node
     .call(dex.config.configureStroke, config.stroke)
     .attr('opacity', config.opacity)
     .style('fill', config.color);
 };
 
-dex.config.configureCircle = function(node, config)
-{
+dex.config.configureCircle = function (node, config) {
   return node
     .call(dex.config.configureShapeStyle, config.style)
     .attr('r', config.radius)
@@ -341,36 +349,32 @@ dex.config.configureCircle = function(node, config)
     .attr('cy', config.center.y);
 };
 
-dex.config.label = function(custom)
-{
-  var config = 
+dex.config.label = function (custom) {
+  var config =
   {
-    'x' : 0,
-    'y' : 0,
-    'transform' : "",
-    'dy' : ".71em",
-    'font' : this.font(),
-    'text' : '',
-    'anchor' : 'end',
-    'color' : 'black',
-    'textLength' : null,
-    'lengthAdjust' : null,
-    'writingMode' : null,
-    'glyphOrientationVertical' : null
+    'x': 0,
+    'y': 0,
+    'transform': "",
+    'dy': ".71em",
+    'font': this.font(),
+    'text': '',
+    'anchor': 'end',
+    'color': 'black'
+    //'textLength': null,
+    //'lengthAdjust': null,
+    //'writingMode': null,
+    //'glyphOrientationVertical': null
   };
-  if (custom)
-  {
+  if (custom) {
     config = dex.object.overlay(custom, config);
   }
   return config;
 };
 
-
-dex.config.configureLabel = function(node, config, text)
-{
+dex.config.configureLabel = function (node, config, text) {
   //console.dir(node);
 
-	dex.config.setAttr(node, "x", config.x);
+  dex.config.setAttr(node, "x", config.x);
   dex.config.setAttr(node, "y", config.y);
   dex.config.setAttr(node, "transform", config.transform);
   dex.config.setAttr(node, "dy", config.dy);
@@ -383,17 +387,15 @@ dex.config.configureLabel = function(node, config, text)
   dex.config.setStyle(node, "writing-mode", config.writingMode);
   dex.config.setStyle(node, "glyph-orientation-vertical", config.glyphOrientationVertical);
 
-  if (typeof config.text != 'undefined')
-  {
+  if (typeof config.text != 'undefined') {
     node.text(dex.config.optionValue(config.text));
   }
 
   return node;
 };
 
-dex.config.configureAxis = function(config)
-{
-	var axis = d3.svg.axis()
+dex.config.configureAxis = function (config) {
+  var axis = d3.svg.axis()
     .ticks(config.tick.count)
     .tickSubdivide(config.tick.subdivide)
     .tickSize(config.tick.size.major, config.tick.size.minor,
@@ -403,17 +405,16 @@ dex.config.configureAxis = function(config)
   // REM: Horrible way of doing this.  Need a function which
   // is more generic and smarter to short circuit stuff like
   // this.  But...for now it does what I want.
-  if (!dex.object.isFunction(config.tick.format))
-  {
+  if (!dex.object.isFunction(config.tick.format)) {
     axis.tickFormat(config.tick.format);
   }
 
   axis
-  .orient(config.orient)
-  .scale(config.scale);
+    .orient(config.orient)
+    .scale(config.scale);
 
-    //axis.scale = config.scale;
-    return axis;
+  //axis.scale = config.scale;
+  return axis;
 };dex.array = {};
 
 /**
